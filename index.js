0cca9cbdd08358705ac4223f0967af07a64a5e66
noeline <noeline.mar@onja.org>
2023-03-30 18:19:31 +0300

    create upload and play audio

--- index.js
+++ index.js
@@ -1,1 +1,117 @@
-console.log('Hello world');
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
+
+// var audioF = new Audio();
+var fileInput = document.getElementById('fileInput');
+
+function loadFile(event) {
+  var files = event.target.files;
+  if (files.length > 0) {
+    var file = files[0];
+    audio.src = URL.createObjectURL(file);
+    audio.load();
+  }
+}
+
+// console.log('audioF::::::', audioF.localName);
+
+const songs = [
+  'Juice WRLD Ft Benny Blanco - Real Shit',
+  'Lil Baby, Lil Durk ft Rodwave - Rich Off Pain',
+  'Polo G – I Know',
+];
+
+let songIndex = 2;
+
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+  title.innerText = song;
+
+  cover.src = `images/${song}.jpg`;
+}
+
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+
+  console.log('audio::::::', audio);
+}
+
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
+}
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+audio.addEventListener('timeupdate', updateProgress);
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+progressContainer.addEventListener('click', setProgress);
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+audio.addEventListener('ended', nextSong);
