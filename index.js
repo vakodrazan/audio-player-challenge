40e11074c1045bf54998811bc254925031b82903
noeline <noeline.mar@onja.org>
2023-03-31 16:31:51 +0300

    Remove unnecessary files and add audio visualiser

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 const musicContainer = document.getElementById('music-container');
 const playBtn = document.getElementById('play');
-const prevBtn = document.getElementById('previous');
-const nextBtn = document.getElementById('next');
 const stopBtn = document.getElementById('stop');
 const uploadTitle = document.getElementById('audio-title');
-
 const audioElm = document.getElementById('audio');
 const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('music-info-title');
 
-const songs = [
-  'Bakozetra-Veloma-Rô',
-  'Mangilatra-By-BAKOZETRA',
-  'Scars-to-your-beautiful-Alessia-Cara',
-  'MAGE4-Rediredy',
-];
-
 function loadFile(event) {
-  let files = event.target.files;
+  const files = event.target.files;
 
   if (files.length > 0) {
-    let file = files[0];
+    const file = files[0];
     const fileName = file?.name.split('.');
     const songTitle = file?.name.replace(
       `.${fileName[fileName.length - 1]}`,
@@ -37,60 +27,27 @@ function loadFile(event) {
   }
 }
 
-let songIndex = 2;
-
-loadSong(songs[songIndex]);
-
-function loadSong(song) {
-  title.innerText = song;
-
-  audioElm.src = `music/${song}.mp3`;
-}
-
 function playSong() {
   musicContainer.classList.add('play');
   playBtn.querySelector('i.fas').classList.remove('fa-play');
   playBtn.querySelector('i.fas').classList.add('fa-pause');
-
   audioElm.play();
+  visualiserAudio();
 }
 
 function pauseSong() {
   musicContainer.classList.remove('play');
   playBtn.querySelector('i.fas').classList.add('fa-play');
   playBtn.querySelector('i.fas').classList.remove('fa-pause');
-
   audioElm.pause();
 }
+
 function stopSong() {
   musicContainer.classList.remove('play');
   playBtn.querySelector('i.fas').classList.add('fa-play');
   playBtn.querySelector('i.fas').classList.remove('fa-pause');
   audioElm.pause();
   audioElm.currentTime = 0;
-}
-
-function prevSong() {
-  songIndex--;
-
-  if (songIndex < 0) {
-    songIndex = songs.length - 1;
-  }
-  loadSong(songs[songIndex]);
-
-  playSong();
-}
-
-function nextSong() {
-  songIndex++;
-
-  if (songIndex > songs.length - 1) {
-    songIndex = 0;
-  }
-
-  loadSong(songs[songIndex]);
-
-  playSong();
 }
 
 function updateProgress(e) {
@@ -103,23 +60,62 @@ function setProgress(e) {
   const width = this.clientWidth;
   const clickX = e.offsetX;
   const duration = audioElm.duration;
-
   audioElm.currentTime = (clickX / width) * duration;
 }
 
 playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play');
-
-  if (isPlaying) {
-    pauseSong();
-  } else {
-    playSong();
-  }
+  isPlaying ? pauseSong() : playSong();
 });
 
+function visualiserAudio() {
+  const context = new AudioContext();
+  const src = context.createMediaElementSource(audioElm);
+  let analyser = context.createAnalyser();
+
+  let canvas = document.getElementById('canvasElm');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  const ctx = canvas.getContext('2d');
+  src.connect(analyser);
+  analyser.connect(context.destination);
+  analyser.fftSize = 256;
+
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  const WIDTH = canvas.width;
+  const HEIGHT = canvas.height;
+
+  const barWidth = (WIDTH / bufferLength) * 2.5;
+  let barHeight;
+  let x = 0;
+
+  function renderFrame() {
+    requestAnimationFrame(renderFrame);
+
+    x = 0;
+
+    analyser.getByteFrequencyData(dataArray);
+
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    for (let i = 0; i < bufferLength; i++) {
+      barHeight = dataArray[i];
+      const r = barHeight + 25 * (i / bufferLength);
+      const g = 250 * (i / bufferLength);
+      const b = 50;
+
+      ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+      ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
+      x += barWidth + 1;
+    }
+  }
+  renderFrame();
+}
+
 stopBtn.addEventListener('click', stopSong);
-prevBtn.addEventListener('click', prevSong);
-nextBtn.addEventListener('click', nextSong);
 audioElm.addEventListener('timeupdate', updateProgress);
 progressContainer.addEventListener('click', setProgress);
-audioElm.addEventListener('ended', nextSong);
